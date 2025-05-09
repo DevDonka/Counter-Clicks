913886d5796d525ea8ab968fac218e53e03eaefa
_DonkaDev <dubanp1211@gmail.com>
2025-05-09 08:57:57 -0500

    Update contador.js

--- contador.js
+++ contador.js
@@ -6,21 +6,17 @@ const contadorDisplay = document.getElementById('contador');
 // Generar gotas de lluvia
 function createRain() {
   const rainContainer = document.querySelector('.rain');
-  const numberOfDrops = 100; // Número de gotas de lluvia
+  const numberOfDrops = 100;
 
   for (let i = 0; i < numberOfDrops; i++) {
     const drop = document.createElement('div');
     drop.classList.add('drop');
-    drop.style.left = `${Math.random() * window.innerWidth}px`; // Posición horizontal aleatoria
-    drop.style.animationDuration = `${Math.random() * 1.5 + 1}s`; // Duración aleatoria para las gotas
+    drop.style.left = `${Math.random() * window.innerWidth}px`;
+    drop.style.animationDuration = `${Math.random() * 1.5 + 1}s`;
     rainContainer.appendChild(drop);
   }
 }
-
-// Iniciar lluvia al cargar la página
 window.onload = createRain;
-
-// Incrementar contador al hacer clic
 clickButton.addEventListener('click', () => {
   contador++;
   contadorDisplay.textContent = `Número de clics: ${contador}`;
